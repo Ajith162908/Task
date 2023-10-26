21447f2209ec7dd73de83d10bc3428984f6cf833
Ajith162908 <ak08092022@gmail.com>
2023-10-26 09:06:04 +0530

    Add the Method

--- src/test/java/org/mm/Class1.java
+++ src/test/java/org/mm/Class1.java
@@ -1,5 +1,30 @@
 package org.mm;
 
 public class Class1 {
+	private void method1() {
+		System.out.println("1");
 
+	}
+	private void method2() {
+		System.out.println("2");
+
+	}
+	private void method3() {
+		System.out.println("3");
+
+	}
+	private void add1() {
+		System.out.println("1");
+
+	}
+	private void add2() {
+		System.out.println("2");
+
+	}
+	private void add3() {
+		System.out.println("3");
+
+	}
+	
+	
 }
